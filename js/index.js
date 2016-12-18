56543891211483b6d88aa49daa3d253f42bc3225
Rob <rdaly1490@gmail.com>
2016-12-18 01:29:58 -0600

    update so user can add own image

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-const inputs = document.querySelectorAll('.controls input');
+const controlInputs = document.querySelectorAll('.controls input');
+const form = document.querySelector('form');
 
 function handleInputUpdate() {
 	const cssUnit = this.dataset.unit || '';
@@ -6,8 +7,34 @@ function handleInputUpdate() {
 	if (this.name === 'brightness') {
 		value = this.value / 100;
 	}
+	console.log(this.name, value, cssUnit)
 	document.documentElement.style.setProperty(`--${this.name}`, value + cssUnit);
 }
 
-inputs.forEach(input => input.addEventListener('change', handleInputUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleInputUpdate));
+function handleFormSubmit(e) {
+	e.preventDefault();
+	const newImageURL = document.querySelector('input[name="url"]').value;
+	if (newImageURL) {
+		checkImageExists(newImageURL, loadNewImage);
+	}
+}
+
+function checkImageExists(url, callback) {
+  var img = new Image();
+  img.onload = () => callback(true, url);
+  img.onerror = () => callback(false);
+  img.src = url;
+}
+
+function loadNewImage(imgExists, url) {
+	const newImageSrc = url || '';
+	if (imgExists && newImageSrc) {
+		document.querySelector('img').src = newImageSrc;
+	} else {
+		alert("Couldn't load image, please verify the link");
+	}
+}
+
+controlInputs.forEach(input => input.addEventListener('change', handleInputUpdate));
+controlInputs.forEach(input => input.addEventListener('mousemove', handleInputUpdate));
+form.addEventListener('submit', handleFormSubmit);
